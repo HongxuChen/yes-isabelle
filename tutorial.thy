5a2f2f0bd32ef944242896983a8c3ba2a7406f5c
Hongxu <hongxuchen1989@gmail.com>
2017-02-20 09:56:26 +0800

    bit more

--- tutorial.thy
+++ tutorial.thy
@@ -1,5 +1,5 @@
-theory test 
-imports Main 
+theory tutorial
+imports Main
 begin
 
 fun conj :: "bool \<Rightarrow> bool \<Rightarrow> bool" where
@@ -103,6 +103,15 @@ lemma "\<forall> x. \<exists> y. x = y" by auto
 lemma "A \<subseteq> B \<inter> C \<Longrightarrow> A \<subseteq> B \<union> C" by auto
 
 lemma "\<lbrakk>\<forall> xs \<in> A. \<exists> ys. xs = ys @ ys; us \<in> A \<rbrakk> \<Longrightarrow> \<exists> n. length us = n + n"
-by fastforce
+  by fastforce
+    
+lemma "\<lbrakk>\<forall> x y. T x y \<or> T y x; \<forall> x y. A x y \<and> A y x \<longrightarrow> x = y; \<forall> x y. T x y \<longrightarrow> A x y \<rbrakk> \<Longrightarrow> 
+\<forall> x y. A x y \<longrightarrow> T x y" by blast
+
+lemma "\<lbrakk> (a::nat)\<le>x+b; 2*x < c\<rbrakk> \<Longrightarrow> 2*a +1 \<le> 2*b +c " by arith
+
+inductive ev :: "nat\<Rightarrow>bool" where 
+  ev0 : "ev 0"
+  | evSS : "ev n \<Longrightarrow> ev (n+2)"
 
 end
